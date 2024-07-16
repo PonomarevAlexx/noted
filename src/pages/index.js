6e1ae60f61c1088a31001c292d5930aa7b654e03
Alex Ponomarev <ponomar3v.alexey@yandex.ru>
2024-07-16 17:10:04 +0300

    настроена apollo client и добавленны несколько запросов

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,16 @@ import Layout from '../components/Layout';
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
+import NotePage from './note';
 
 const Pages = () => {
   return (
     <Router>
       <Layout>
+        <Route exact path="/" component={Home} />
         <Route path="/mynotes" component={MyNotes} />
         <Route path="/favorites" component={Favorites} />
-        <Route exact path="/" component={Home} />
+        <Route path="/note/:id" component={NotePage} />
       </Layout>
     </Router>
   );
