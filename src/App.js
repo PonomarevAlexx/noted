a5357b96bc71f1bb7fe7eb2a86b0027965d64658
Alex Ponomarev <ponomar3v.alexey@yandex.ru>
2024-07-18 17:55:19 +0300

    добавленна аутентификация и состояния

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 import GlobalStyle from './components/GlobalStyle';
 import Pages from './pages';
 
+// Настраиваем API URI и кэш
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
+// Проверяем наличие токена и возвращаем заголовки в контекст
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
+// Создаем клиент Apollo
 const client = new ApolloClient({
-  uri,
+  link: authLink.concat(httpLink),
   cache,
+  resolvers: {},
   connectToDevTools: true
 });
+
+// Проверяем наличие локального токена
+const data = {
+  isLoggedIn: !!localStorage.getItem('token')
+};
+// Записываем кэшированные данные при начальной загрузке
+cache.writeData({ data });
+// Записываем данные кэша после его сброса
+client.onResetStore(() => cache.writeData({ data }));
 
 const App = () => {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from '../img/logo.svg';
 import styled from 'styled-components';
+import { useQuery, gql } from '@apollo/client';
+import { Link, withRouter } from 'react-router-dom';
+
+import ButtonAsLink from './ButtonAsLink';
 
 const HeaderBar = styled.header`
   width: 100%;
@@ -19,13 +23,50 @@ const LogoText = styled.h1`
   display: inline;
 `;
 
-const Header = () => {
+const UserState = styled.div`
+  margin-left: auto;
+`;
+
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+const Header = props => {
+  const { data, client } = useQuery(IS_LOGGED_IN);
+
   return (
     <HeaderBar>
       <img src={logo} alt="Notedly Logo" height="40" />
       <LogoText>Notedly</LogoText>
+      {/* Если авторизован, отображаем ссылку log out, в противном
+    случае отображаем варианты sign in и sign up */}
+      <UserState>
+        {data.isLoggedIn ? (
+          <ButtonAsLink
+            onClick={() => {
+              // Удаляем токен
+              localStorage.removeItem('token');
+              // Очищаем кэш приложения
+              client.resetStore();
+              // Обновляем локальное состояние
+              client.writeData({ data: { isLoggedIn: false } });
+              // Перенаправляем пользователя на домашнюю страницу
+              props.history.push('/');
+            }}
+          >
+            Logout
+          </ButtonAsLink>
+        ) : (
+          <p>
+            <Link to={'/signin'}>Sign In</Link> or{' '}
+            <Link to={'/signup'}>Sign Up</Link>
+          </p>
+        )}
+      </UserState>
     </HeaderBar>
   );
 };
 
-export default Header;
+export default withRouter(Header);
